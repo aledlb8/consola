6750596464c34d88a34c4fbb86e79f32e18557ad
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2022-07-12 01:34:33 +0200

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+import consola from "./consola.ts";
+
+consola.clear();
+consola.log("Hello World");
+consola.log();
+consola.success("Hello World", "text");
+consola.success("Hello World", "background");
+consola.success("Hello World", "icon");
+consola.log();
+consola.error("Hello World", "text");
+consola.error("Hello World", "background");
+consola.error("Hello World", "icon");
+consola.log();
+consola.warn("Hello World", "text");
+consola.warn("Hello World", "background");
+consola.warn("Hello World", "icon");
+consola.log();
+consola.info("Hello World", "text");
+consola.info("Hello World", "background");
+consola.info("Hello World", "icon");
+consola.log();
+consola.debug("Hello World", "text");
+consola.debug("Hello World", "background");
+consola.debug("Hello World", "icon");
