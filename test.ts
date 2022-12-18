690a8162f0a060a7f50281a035521561f58af07b
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2022-12-17 23:46:30 -0400

    consola.log - removed
    Useless

--- test.ts
+++ test.ts
@@ -2,24 +2,22 @@
 import consola from "./consola.ts";
 
 consola.clear();
-consola.log("Hello World");
-consola.log();
 consola.success("text", "Hello World");
 consola.success("background", "helo World");
 consola.success("icon", "Hello World");
-consola.log();
+console.log();
 consola.error("text", "Hello World");
 consola.error("background", "helo World");
 consola.error("icon", "Hello World");
-consola.log();
+console.log();
 consola.warn("text", "Hello World");
 consola.warn("background", "helo World");
 consola.warn("icon", "Hello World");
-consola.log();
+console.log();
 consola.info("text", "Hello World");
 consola.info("background", "helo World");
 consola.info("icon", "Hello World");
-consola.log();
+console.log();
 consola.debug("text", "Hello World");
 consola.debug("background", "helo World");
 consola.debug("icon", "Hello World");
