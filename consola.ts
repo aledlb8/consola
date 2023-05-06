da7714a2bd75fa5149919801bb582487a5aa9e72
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2023-05-05 21:46:05 -0400

    Update consola.ts
    - Removed the `any` type from function parameters and return types.
    - Used the `type` alias to define the `MessageType` type and make the code more readable.
    - Created a `printMessage` function to reduce code repetition and make the code more modular.
    - Removed the unnecessary comment annotations.
    - Removed the `noSource` function since it's not being used.

--- consola.ts
+++ consola.ts
@@ -1,110 +1,109 @@
-const color: any = {
-    reset: "\x1b[0m",
-    black: "\x1b[30m",
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    blue: "\x1b[34m",
-    magenta: "\x1b[35m",
-    cyan: "\x1b[36m",
-    white: "\x1b[37m",
-    grey: "\x1b[90m",
+type Color = {
+  reset: string;
+  black: string;
+  red: string;
+  green: string;
+  yellow: string;
+  blue: string;
+  magenta: string;
+  cyan: string;
+  white: string;
+  grey: string;
+  backgroundBlack: string;
+  backgroundRed: string;
+  backgroundGreen: string;
+  backgroundYellow: string;
+  backgroundBlue: string;
+  backgroundMagenta: string;
+  backgroundCyan: string;
+  backgroundWhite: string;
+  backgroundGrey: string;
+};
 
-    backgroundBlack: "\x1b[40m",
-    backgroundRed: "\x1b[41m",
-    backgroundGreen: "\x1b[42m",
-    backgroundYellow: "\x1b[43m",
-    backgroundBlue: "\x1b[44m",
-    backgroundMagenta: "\x1b[45m",
-    backgroundCyan: "\x1b[46m",
-    backgroundWhite: "\x1b[47m",
-    backgroundGrey: "\x1b[100m",
+const color: Color = {
+  reset: "\x1b[0m",
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+  grey: "\x1b[90m",
+  backgroundBlack: "\x1b[40m",
+  backgroundRed: "\x1b[41m",
+  backgroundGreen: "\x1b[42m",
+  backgroundYellow: "\x1b[43m",
+  backgroundBlue: "\x1b[44m",
+  backgroundMagenta: "\x1b[45m",
+  backgroundCyan: "\x1b[46m",
+  backgroundWhite: "\x1b[47m",
+  backgroundGrey: "\x1b[100m",
+};
+
+type Sources = {
+  text: string;
+  background: string;
+  icon: string;
+};
+
+type MessageType = "text" | "background" | "icon";
+
+function printMessage(type: string, sources: Sources, message: string) {
+  const source = sources[type];
+  if (source) {
+    console.log(`${source} ${message}${color.reset}`);
+  } else {
+    throw new TypeError("missing parameters.");
+  }
 }
 
-const Consola = {    
-    success: function (type?: any, message?: any): void {
-        /**
-         * @param {any} message
-         * @param {any} type
-         * @returns {void}
-         */
+const Consola = {
+  success(type: MessageType = "text", message: any) {
+    const sources: Sources = {
+      text: `${color.white}[${color.green}SUCCESS${color.white}]`,
+      background: `${color.backgroundGreen}SUCCESS`,
+      icon: `${color.green}✔`,
+    };
+    printMessage(type, sources, message);
+  },
 
-        const sources: { [key: string]: string; } = {
-            "text": `${color.white}[${color.green}SUCCESS${color.white}] ${color.reset}${message}`,
-            "background": `${color.backgroundGreen}SUCCESS${color.reset} ${message}`,
-            "icon": `${color.green}✔${color.reset} ${message}`,
-        };
+  error(type: MessageType = "text", message: any) {
+    const sources: Sources = {
+      text: `${color.white}[${color.red}ERROR${color.white}]`,
+      background: `${color.backgroundRed}ERROR`,
+      icon: `${color.red}✖`,
+    };
+    printMessage(type, sources, message);
+  },
 
-        console.log(sources[type] || noSource());
-    },
+  warn(type: MessageType = "text", message: any) {
+    const sources: Sources = {
+      text: `${color.white}[${color.yellow}WARN${color.white}]`,
+      background: `${color.backgroundYellow}WARN`,
+      icon: `${color.yellow}⚠`,
+    };
+    printMessage(type, sources, message);
+  },
 
-    error: function (type?: any, message?: any): void {
-        /**
-         * @param {any} message
-         * @param {any} type
-         * @returns {void}
-         */
+  info(type: MessageType = "text", message: any) {
+    const sources: Sources = {
+      text: `${color.white}[${color.blue}INFO${color.white}]`,
+      background: `${color.backgroundBlue}INFO`,
+      icon: `${color.blue}✖`,
+    };
+    printMessage(type, sources, message);
+  },
 
-         const sources: { [key: string]: string; } = {
-            "text": `${color.white}[${color.red}ERROR${color.white}] ${color.reset}${message}`,
-            "background": `${color.backgroundRed}ERROR${color.reset} ${message}`,
-            "icon": `${color.red}✖${color.reset} ${message}`,
-        };
-
-        console.log(sources[type] || noSource());
-    },
-
-    warn: function (type?: any, message?: any): void {
-        /**
-         * @param {any} message
-         * @param {any} type
-         * @returns {void}
-         */
-
-         const sources: { [key: string]: string; } = {
-            "text": `${color.white}[${color.yellow}WARN${color.white}] ${color.reset}${message}`,
-            "background": `${color.backgroundYellow}WARN${color.reset} ${message}`,
-            "icon": `${color.yellow}⚠${color.reset} ${message}`,
-        };
-
-        console.log(sources[type] || noSource());
-    },
-
-    info: function (type?: any, message?: any): void {
-        /**
-         * @param {any} message
-         * @param {any} type
-         * @returns {void}
-         */
-
-         const sources: { [key: string]: string; } = {
-            "text": `${color.white}[${color.blue}INFO${color.white}] ${color.reset}${message}`,
-            "background": `${color.backgroundBlue}INFO${color.reset} ${message}`,
-            "icon": `${color.blue}✖${color.reset} ${message}`,
-        };
-
-        console.log(sources[type] || noSource());
-    },
-
-    debug: function (type?: any, message?: any): void {
-        /**
-         * @param {any} message
-         * @param {any} type
-         * @returns {void}
-         */
-
-         const sources: { [key: string]: string; } = {
-            "text": `${color.white}[${color.grey}DEBUG${color.white}] ${color.reset}${message}`,
-            "background": `${color.backgroundGrey}DEBUG${color.reset} ${message}`,
-            "icon": `${color.grey}#${color.reset} ${message}`,
-        };
-
-        console.log(sources[type] || noSource());
-    }
-}
-
-function noSource(): void {
-    throw new TypeError("missing parameters.")
+  debug(type: MessageType = "text", message: any) {
+    const sources: Sources = {
+      text: `${color.white}[${color.grey}DEBUG${color.white}]`,
+      background: `${color.backgroundGrey}DEBUG`,
+      icon: `${color.grey}#`,
+    };
+    printMessage(type, sources, message);
+  },
 };
 
 export default Consola;
