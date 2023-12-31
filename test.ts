aaa083d44ce82126c73fe4967697631deeb614da
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2023-12-31 10:33:47 -0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,12 @@
 // @ts-ignore
 import consola from "./consola.ts";
 
-consola.clear();
-consola.success("text", "Hello World");
-consola.success("background", "helo World");
-consola.success("icon", "Hello World");
+consola.success("Success");
 console.log();
-consola.error("text", "Hello World");
-consola.error("background", "helo World");
-consola.error("icon", "Hello World");
+consola.error("Error");
 console.log();
-consola.warn("text", "Hello World");
-consola.warn("background", "helo World");
-consola.warn("icon", "Hello World");
+consola.warn("Warn");
 console.log();
-consola.info("text", "Hello World");
-consola.info("background", "helo World");
-consola.info("icon", "Hello World");
+consola.info("Info");
 console.log();
-consola.debug("text", "Hello World");
-consola.debug("background", "helo World");
-consola.debug("icon", "Hello World");
+consola.debug("Debug");
