a475f66545cc47067b7fc68db9dd70efadbb3307
aledlb8 <74106092+aledlb8@users.noreply.github.com>
2023-12-31 10:33:37 -0800

    Update consola.ts

--- consola.ts
+++ consola.ts
@@ -1,55 +1,61 @@
-enum TextColor {
-  Reset = "\x1b[0m",
-  Black = "\x1b[30m",
-  Red = "\x1b[31m",
-  Green = "\x1b[32m",
-  Yellow = "\x1b[33m",
-  Blue = "\x1b[34m",
-  Magenta = "\x1b[35m",
-  Cyan = "\x1b[36m",
-  White = "\x1b[37m",
-  Grey = "\x1b[90m",
-}
+const LogLevel = {
+  Success: "SUCCESS",
+  Error: "ERROR",
+  Warn: "WARN",
+  Info: "INFO",
+  Debug: "DEBUG",
+} as const;
 
-enum BackgroundColor {
-  Black = "\x1b[40m",
-  Red = "\x1b[41m",
-  Green = "\x1b[42m",
-  Yellow = "\x1b[43m",
-  Blue = "\x1b[44m",
-  Magenta = "\x1b[45m",
-  Cyan = "\x1b[46m",
-  White = "\x1b[47m",
-  Grey = "\x1b[100m",
-}
+const TextColor = {
+  Reset: "\x1b[0m",
+  Black: "\x1b[30m",
+  Red: "\x1b[31m",
+  Green: "\x1b[32m",
+  Yellow: "\x1b[33m",
+  Blue: "\x1b[34m",
+  Magenta: "\x1b[35m",
+  Cyan: "\x1b[36m",
+  White: "\x1b[37m",
+  Grey: "\x1b[90m",
+} as const;
+
+type ObjectValues<T> = T[keyof T];
+type TypeLogLevel = ObjectValues<typeof LogLevel>;
+type TypeColor = ObjectValues<typeof TextColor>;
 
 function getCurrentTime(): string {
-  const now = new Date();
+  const now: Date = new Date();
   const hours = now.getHours().toString().padStart(2, "0");
   const minutes = now.getMinutes().toString().padStart(2, "0");
   const seconds = now.getSeconds().toString().padStart(2, "0");
   return `${TextColor.White}[${TextColor.Cyan}${hours}${TextColor.White}:${TextColor.Cyan}${minutes}${TextColor.White}:${TextColor.Cyan}${seconds}${TextColor.White}]${TextColor.Reset}`;
 }
 
-interface Logger {
+type Logger = {
   (message: string, ...optionalParams: never[]): void;
 }
 
-const createLogger = (level: string, color: TextColor): Logger => {
+type Consola = {
+  success: Logger
+  error: Logger;
+  warn: Logger;
+  info: Logger;
+  debug: Logger;
+}
+
+const createLogger = (level: TypeLogLevel, color: TypeColor): Logger => {
   return (message?: string, ...optionalParams: never[]): void => {
     const text: string = `${getCurrentTime()} ${TextColor.White}[${color}${level}${TextColor.White}] ${TextColor.Reset}${message !== undefined ? message : ' '}`
     console.log(text, ...optionalParams);
-
-    FileManager.writeFile('logs.txt', `${text}\n`);
   };
 };
 
-const consola = {
-  success: createLogger("SUCCESS", TextColor.Green),
-  error: createLogger("ERROR", TextColor.Red),
-  warn: createLogger("WARN", TextColor.Yellow),
-  info: createLogger("INFO", TextColor.Blue),
-  debug: createLogger("DEBUG", TextColor.Grey),
+const consola: Consola = {
+  success: createLogger(LogLevel.Success, TextColor.Green),
+  error: createLogger(LogLevel.Error, TextColor.Red),
+  warn: createLogger(LogLevel.Error, TextColor.Yellow),
+  info: createLogger(LogLevel.Error, TextColor.Blue),
+  debug: createLogger(LogLevel.Error, TextColor.Grey),
 };
 
 export default consola;
